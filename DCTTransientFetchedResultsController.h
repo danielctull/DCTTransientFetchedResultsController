7acec453c648ce1a1d5e132f2e128db4f163286b
Daniel Tull <dt@danieltull.co.uk>
2012-01-14 00:23:45 +0000

    Initial commit

---
+++ DCTTransientFetchedResultsController.h
@@ -0,0 +1,12 @@
+//
+//  DCTTransientFetchedResultsController.h
+//  DCTTransientFetchedResultsController
+//
+//  Created by Daniel Tull on 13.01.2012.
+//  Copyright (c) 2012 Daniel Tull Limited. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface DCTTransientFetchedResultsController : NSFetchedResultsController
+@end
---
+++ DCTTransientFetchedResultsController.m
@@ -0,0 +1,13 @@
+//
+//  DCTTransientFetchedResultsController.m
+//  DCTTransientFetchedResultsController
+//
+//  Created by Daniel Tull on 13.01.2012.
+//  Copyright (c) 2012 Daniel Tull Limited. All rights reserved.
+//
+
+#import "DCTTransientFetchedResultsController.h"
+
+@implementation DCTTransientFetchedResultsController
+
+@end
